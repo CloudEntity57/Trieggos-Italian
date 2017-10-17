d7cfaac243585ac5a97aa7ee0a7ca9b743ca096a
Josh Foster <josh@allenb.com>
2017-10-17 17:31:43 -0500

    responsive

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,12 +5,22 @@ export default class Nav extends Component{
     return(
       <div className="navigation">
         <div className="restaurant_logo">Trieggo's</div>
-        <ul className="navigation_list">
+        <ul className="navigation_list nav_full">
           <li className="nav_item">CONTACT</li>
           <li className="nav_item">NEWS</li>
           <li className="nav_item">RECIPES</li>
           <li className="nav_item">SHOP</li>
         </ul>
+        <ul className="navigation_list nav_mobile">
+          <div className='nav_mobile_opacity'></div>
+          <li className="nav_item">CONTACT</li>
+          <li className="nav_item">NEWS</li>
+          <li className="nav_item">RECIPES</li>
+          <li className="nav_item">SHOP</li>
+        </ul>
+        <div className="nav_mobile_btn">
+          <i className="fa fa-bars"></i>
+        </div>
       </div>
     )
   }
